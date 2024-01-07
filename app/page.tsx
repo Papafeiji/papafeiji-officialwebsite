1126220280c936a5e99774170ddc211d2be3b516
chenproton <chenproton@gmail.com>
2024-01-07 21:54:33 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-           <Image
-              src="/papafeiji.png"
-              alt="papafeiji Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           爬爬飞记&nbsp;
           <code className="font-mono font-bold">可以记录你生活中发生的一切，当你的完美记忆助手</code>
@@ -23,9 +15,9 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            {' '}
             <Image
-              src="/papafeiji.png"
+              src="/papafeiji.svg"
               alt="papafeiji Logo"
               className="dark:invert"
               width={100}
