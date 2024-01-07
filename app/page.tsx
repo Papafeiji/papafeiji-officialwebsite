68179e68f9fc27b5e5c41f701a2c00e6a482e462
chenproton <chenproton@gmail.com>
2024-01-08 00:21:05 +0800

    更改文案

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-           我们尊重您的隐私,只有您允许后 AI 助手才能访问它们
+           我们尊重您的隐私，只有您允许后 LLM 模型才能访问数据
           </p>
         </a>
       </div>
