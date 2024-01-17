517efab3e02792411eb0b997143f3b525224e619
chenproton <chenproton@gmail.com>
2024-01-18 06:32:17 +0800

    回退

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
 import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-center"> {/* 添加了 text-center 类 */}
-        <p style={{ backgroundColor: '#B58D5E' }} className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 lg:dark:bg-zinc-800/30 text-center"> {/* 添加了 text-center 类 */}
-           &nbsp;
-           <code className="font-mono font-bold">爬爬飞记可以记录你生活中发生的一切，当你的完美记忆助手</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.papafeiji.cn/" 
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-          </a>
-        </div>
-      </div>
-
-
-/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"> 
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-center"> 
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p style={{ backgroundColor: '#B58D5E' }} className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 lg:dark:bg-zinc-800/30">
+           爬爬飞记&nbsp;
+           <code className="font-mono font-bold">可以记录你生活中发生的一切，当你的完美记忆助手</code>
            &nbsp;
            <code className="font-mono font-bold">爬爬飞记可以记录你生活中发生的一切，当你的完美记忆助手</code>
         </p>
@@ -40,13 +20,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-<<<<<<< HEAD
-*/ 
-
-      {/* 添加一个空行 */}
-      <div style={{ height: '10px', margin: '0 0 1rem 0' }} />
-
-
 <div className="relative flex place-items-center 
     before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] 
     before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 
@@ -64,20 +37,6 @@ export default function Home() {
       priority
     />
 </div>
-=======
->>>>>>> parent of 32df0ef (更改背景)
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] filter blur-10px"
-          src="/papafeiji.png"
-          alt="Logo"
-          width={300}
-          height={300}
-          priority
-        />
-      </div>
-
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://www.papafeiji.cn/"
@@ -86,18 +45,17 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            进入小程序{' '}
+            下载应用{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            从微信小程序、公众号关注我们
+            从微信小程序、AppStore、各大安卓商店下载
           </p>
         </a>
-
         <a
-          href="https://www.xiaohongshu.com/user/profile/62f09aef000000001f0076d3"
+          href="https://www.papafeiji.cn/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -112,7 +70,6 @@ export default function Home() {
             使用这些技巧与窍门，让爬爬飞记更加好用
           </p>
         </a>
-
         <a
           href="https://www.papafeiji.cn/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -129,9 +86,8 @@ export default function Home() {
             加入我们的用户社群，提出您的问题与建议
           </p>
         </a>
-
         <a
-          href="https://mp.weixin.qq.com/s/q38HwnPuV1U9Us0WBa7efQ"
+          href="https://www.papafeiji.cn/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
