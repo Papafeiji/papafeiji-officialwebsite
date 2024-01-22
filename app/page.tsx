3d7d86ac0f86475127b828ee34aa2ee1259460ae
chenproton <chenproton@gmail.com>
2024-01-22 10:54:15 +0800

    更改文案

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
           </p>
         </a>
         <a
-          href="https://www.papafeiji.cn/"
+          href="https://www.xiaohongshu.com/user/profile/60ac3cfa0000000001003edf"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -85,7 +85,7 @@ export default function Home() {
           </p>
         </a>
         <a
-          href="https://www.papafeiji.cn/"
+          href="https://mp.weixin.qq.com/s/q38HwnPuV1U9Us0WBa7efQ"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
