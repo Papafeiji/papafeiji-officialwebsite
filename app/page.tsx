3ad89c0b84c0e0dd995245562ff849987ae6ec7c
chenproton <chenproton@gmail.com>
2024-01-26 11:01:49 +0800

    更改跳转地址

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
           </p>
         </a>
         <a
-          href="https://mp.weixin.qq.com/s/q38HwnPuV1U9Us0WBa7efQ"
+          href="https://news.papafeiji.cn/privacy-police"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
